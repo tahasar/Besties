1b4c529b3901f6c7a3dddf95ffa8beeebec6518e
Serhat Kıldacı <96617749+serhatkildaci@users.noreply.github.com>
2023-06-15 13:56:13 +0300

    Photon component added

--- Assets/Scripts/UnitMovement.cs
+++ Assets/Scripts/UnitMovement.cs
@@ -1,19 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.UIElements;
-using Photon.Realtime;
 using Photon.Pun;
 
-public class UnitMovement : MonoBehaviourPun
+public class UnitMovement : MonoBehaviourPun, IPunObservable
 {
     private Camera myCam;
     private NavMeshAgent myAgent;
-
     public LayerMask ground;
-
     private List<GameObject> unitsToFormation = new List<GameObject>();
 
     [SerializeField] private int _radius = 1;
@@ -33,54 +28,76 @@ public class UnitMovement : MonoBehaviourPun
     // Update is called once per frame
     void Update()
     {
-        if (photonView.IsMine)
+        if (!photonView.IsMine)
+            return;
+
+        if (Input.GetMouseButtonDown(1))
         {
-            if (Input.GetMouseButtonDown(1))
+            unitsToFormation = UnitSelections.Instance.unitsSelected;
+
+            RaycastHit hit;
+            Ray ray = myCam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, ground))
             {
-                unitsToFormation = UnitSelections.Instance.unitsSelected;
-
-                RaycastHit hit;
-                Ray ray = myCam.ScreenPointToRay(Input.mousePosition);
-
-                if (Physics.Raycast(ray, out hit, Mathf.Infinity, ground))
+                if (unitsToFormation.Count > 1)
                 {
-                    if (unitsToFormation.Count > 1)
-                    {
-                        // Update the positions of all units in the formation
-                        for (int i = 0; i < unitsToFormation.Count; i++)
-                        {
-                            var angle = i * Mathf.PI * (2 * _rotations) / unitsToFormation.Count + (i % 2 != 0 ? _nthOffset : 0);
+                    var amountPerRing = unitsToFormation.Count / _rings;
+                    var ringOffset = 2f;
 
-                            var radius = _radius + _ringOffset + i * _radiusGrowthMultiplier;
+                    if (unitsToFormation.Count % _rings != 0)
+                    {
+                        amountPerRing++;
+                        ringOffset += _ringOffset;
+                    }
+
+                    for (int i = 0; i < _rings; i++)
+                    {
+                        for (var j = 0; j < amountPerRing; j++)
+                        {
+                            var angle = j * Mathf.PI * (2 * _rotations) / amountPerRing + (i % 2 != 0 ? _nthOffset : 0);
+                            var radius = _radius + ringOffset + j * _radiusGrowthMultiplier;
                             var x = Mathf.Cos(angle) * radius;
                             var z = Mathf.Sin(angle) * radius;
 
                             var pos = new Vector3(hit.point.x + x, 0, hit.point.z + z);
 
-                            // Update the position of the unit on all other clients
-                            photonView.RPC("UpdatePosition", RpcTarget.All, pos, photonView.ViewID, i);
-
-                            // Update the position of the unit on the local client
-                            unitsToFormation[i].transform.position = pos;
+                            photonView.RPC("SetDestinationRPC", RpcTarget.All, pos, j);
                         }
-                    }
-                    else
-                    {
-                        // Update the position of the unit on all other clients
-                        photonView.RPC("UpdatePosition", RpcTarget.All, hit.point, photonView.ViewID, 0);
 
-                        // Update the position of the unit on the local client
-                        myAgent.SetDestination(hit.point);
+                        ringOffset += _ringOffset;
                     }
+                }
+                else
+                {
+                    photonView.RPC("SetDestinationRPC", RpcTarget.All, hit.point, -1);
                 }
             }
         }
     }
 
     [PunRPC]
-    void UpdatePosition(Vector3 position, int viewID, int unitIndex)
+    private void SetDestinationRPC(Vector3 destination, int index)
     {
-        GameObject unit = PhotonView.Find(viewID).gameObject;
-        unit.transform.position = position;
+        if (index >= 0 && index < unitsToFormation.Count)
+        {
+            unitsToFormation[index].GetComponent<NavMeshAgent>().SetDestination(destination);
+        }
+        else if (index == -1)
+        {
+            myAgent.SetDestination(destination);
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(transform.position);
+        }
+        else
+        {
+            transform.position = (Vector3)stream.ReceiveNext();
+        }
     }
 }
