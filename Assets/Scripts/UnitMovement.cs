4872c9d696e8e68b3ddcdf8b787ab849b622b837
tahasar <taha_asar100@hotmail.com>
2023-06-15 17:24:21 +0300

    Update UnitMovement.cs

--- Assets/Scripts/UnitMovement.cs
+++ Assets/Scripts/UnitMovement.cs
@@ -63,9 +63,7 @@ public class UnitMovement : MonoBehaviour
                             var z = Mathf.Sin(angle) * radius;
 
                             var pos = new Vector3(hit.point.x + x, 0, hit.point.z + z);
-                                
-                           
-
+                            
                             unitsToFormation[j].GetComponent<NavMeshAgent>().SetDestination(pos);
                         }
 
