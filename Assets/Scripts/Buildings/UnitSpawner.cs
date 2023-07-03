3fd2847aec687317133ddec0e71297229f65948a
tahasar <taha_asar100@hotmail.com>
2023-07-03 18:54:00 +0300

    Unit spawn queues

--- Assets/Scripts/Buildings/UnitSpawner.cs
+++ Assets/Scripts/Buildings/UnitSpawner.cs
@@ -1,16 +1,41 @@
-using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using Mirror;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class UnitSpawner : NetworkBehaviour, IPointerClickHandler
 {
     [SerializeField] private Health health = null;
-    [SerializeField] private GameObject unitPrefab = null;
+    [SerializeField] private Unit unitPrefab = null;
     [SerializeField] private Transform unitSpawnPoint = null;
+    [SerializeField] private TMP_Text remainingUnitsText = null;
+    [SerializeField] private Image unitProgressImage = null;
+    [SerializeField] private int maxUnitQueue = 5;
+    [SerializeField] private float spawnMoveRange = 7f;
+    [SerializeField] private float unitSpawnDuration = 5f;
+
+    [SyncVar(hook = nameof(ClientHandleQueuedUnitsUpdated))]
+    private int queuedUnits;
+    [SyncVar]
+    private float unitTimer;
+
+    private float progressImageVelocity;
+
+    private void Update()
+    {
+        if (isServer)
+        {
+            ProduceUnits();
+        }
+
+        if (isClient)
+        {
+            UpdateTimerDisplay();
+        }
+    }
 
     #region Server
 
@@ -23,7 +48,33 @@ public class UnitSpawner : NetworkBehaviour, IPointerClickHandler
     {
         health.ServerOnDie -= ServerHandleDie;
     }
-    
+
+    [Server]
+    private void ProduceUnits()
+    {
+        if (queuedUnits == 0) { return; }
+
+        unitTimer += Time.deltaTime;
+
+        if (unitTimer < unitSpawnDuration) { return; }
+
+        GameObject unitInstance = Instantiate(
+            unitPrefab.gameObject,
+            unitSpawnPoint.position,
+            unitSpawnPoint.rotation);
+
+        NetworkServer.Spawn(unitInstance, connectionToClient);
+
+        Vector3 spawnOffset = Random.insideUnitSphere * spawnMoveRange;
+        spawnOffset.y = unitSpawnPoint.position.y;
+
+        UnitMovement unitMovement = unitInstance.GetComponent<UnitMovement>();
+        unitMovement.ServerMove(unitSpawnPoint.position + spawnOffset);
+
+        queuedUnits--;
+        unitTimer = 0f;
+    }
+
     [Server]
     private void ServerHandleDie()
     {
@@ -33,29 +84,53 @@ public class UnitSpawner : NetworkBehaviour, IPointerClickHandler
     [Command]
     private void CmdSpawnUnit()
     {
-        GameObject unitInstance = Instantiate(unitPrefab, unitSpawnPoint.position, unitSpawnPoint.rotation);
-        
-        NetworkServer.Spawn(unitInstance, connectionToClient);
+        if (queuedUnits == maxUnitQueue) { return; }
+
+        RTSPlayer player = connectionToClient.identity.GetComponent<RTSPlayer>();
+
+        if (player.GetResources() < unitPrefab.GetResourceCost()) { return; }
+
+        queuedUnits++;
+
+        player.SetResources(player.GetResources() - unitPrefab.GetResourceCost());
     }
 
     #endregion
 
     #region Client
-    
 
-    public void OnPointerClick(PointerEventData eventData)
+    private void UpdateTimerDisplay()
     {
-        if(eventData.button == PointerEventData.InputButton.Left)
+        float newProgress = unitTimer / unitSpawnDuration;
+
+        if (newProgress < unitProgressImage.fillAmount)
         {
-            if (!isOwned)
-            {
-                return;
-            }
-            CmdSpawnUnit();
+            unitProgressImage.fillAmount = newProgress;
+        }
+        else
+        {
+            unitProgressImage.fillAmount = Mathf.SmoothDamp(
+                unitProgressImage.fillAmount,
+                newProgress,
+                ref progressImageVelocity,
+                0.1f
+            );
         }
     }
 
-    #endregion
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button != PointerEventData.InputButton.Left) { return; }
 
-    
+        if (!hasAuthority) { return; }
+
+        CmdSpawnUnit();
+    }
+
+    private void ClientHandleQueuedUnitsUpdated(int oldUnits, int newUnits)
+    {
+        remainingUnitsText.text = newUnits.ToString();
+    }
+
+    #endregion
 }
--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -7,101 +7,23 @@ using UnityEngine.Events;
 
 public class Unit : NetworkBehaviour
 {
-    [SerializeField] private float floatSpeed = 1f;
-    [SerializeField] private float floatMagnitude = 0.5f;
-    private float phaseOffset;
-    private Vector3 startPosition;
-
-
+    [SerializeField] private int resourceCost = 10;
     [SerializeField] private Health health = null;
     [SerializeField] private UnitMovement unitMovement = null;
     [SerializeField] private Targeter targeter = null;
-
     [SerializeField] private UnityEvent onSelected = null;
-    [SerializeField] private UnityEvent onDeselect = null;
-    // Health and Attack Stats
-    [SerializeField] private int maxHealth = 100;
-    [SyncVar(hook = nameof(HandleHealthUpdated))] private int currentHealth;
-
-    [SerializeField] private int attackDamage = 10;
-    [SerializeField] private float attackRange = 2f;
-    [SerializeField] private float attackCooldown = 1f;
-    private float lastAttackTime = 0f;
-
-    [SerializeField] private string unitName;
+    [SerializeField] private UnityEvent onDeselected = null;
 
     public static event Action<Unit> ServerOnUnitSpawned;
     public static event Action<Unit> ServerOnUnitDespawned;
-    
+
     public static event Action<Unit> AuthorityOnUnitSpawned;
     public static event Action<Unit> AuthorityOnUnitDespawned;
 
-    #region Server
-
-    public override void OnStartServer()
+    public int GetResourceCost()
     {
-        currentHealth = maxHealth;
-        //_healthbar.UpdateHealthBar(maxHealth,currentHealth);
-        ServerOnUnitSpawned?.Invoke(this);
-        
-        health.ServerOnDie += ServerHandleDie;
+        return resourceCost;
     }
-
-    public override void OnStopServer()
-    {
-        health.ServerOnDie -= ServerHandleDie;
-        
-        ServerOnUnitDespawned?.Invoke(this);
-    }
-    
-    [Server]
-    private void ServerHandleDie()
-    {
-        NetworkServer.Destroy(gameObject);
-    }
-
-    [Server]
-    public void DealDamage(Unit target)
-    {
-        if (target == null || !target.isOwned)
-            return;
-        
-        if (!CanAttack())
-            return;
-        
-        target.TakeDamage(attackDamage);
-        lastAttackTime = Time.time;
-    }
-
-    [Server]
-    private void TakeDamage(int damageAmount)
-    {
-        if (currentHealth <= 0)
-            return;
-        
-        currentHealth -= damageAmount;
-        
-        if (currentHealth <= 0)
-        {
-            // Unit destroyed
-            NetworkServer.Destroy(gameObject);
-        }
-
-        
-    }
-
-    private void HandleHealthUpdated(int oldValue, int newValue)
-    {
-        //_healthbar.UpdateHealthBar(maxHealth,currentHealth);
-
-    }
-
-    private bool CanAttack()
-    {
-        return Time.time - lastAttackTime >= attackCooldown;
-    }
-
-    #endregion
 
     public UnitMovement GetUnitMovement()
     {
@@ -113,48 +35,58 @@ public class Unit : NetworkBehaviour
         return targeter;
     }
 
+    #region Server
+
+    public override void OnStartServer()
+    {
+        ServerOnUnitSpawned?.Invoke(this);
+
+        health.ServerOnDie += ServerHandleDie;
+    }
+
+    public override void OnStopServer()
+    {
+        health.ServerOnDie -= ServerHandleDie;
+
+        ServerOnUnitDespawned?.Invoke(this);
+    }
+
+    [Server]
+    private void ServerHandleDie()
+    {
+        NetworkServer.Destroy(gameObject);
+    }
+
+    #endregion
+
     #region Client
 
     public override void OnStartAuthority()
     {
-
         AuthorityOnUnitSpawned?.Invoke(this);
-        phaseOffset = UnityEngine.Random.Range(0f, Mathf.PI * 2f);
-        startPosition = transform.position;    }
+    }
 
     public override void OnStopClient()
     {
-        if (!isOwned)
-        {
-            return;
-        }
+        if (!hasAuthority) { return; }
 
         AuthorityOnUnitDespawned?.Invoke(this);
     }
 
-    private void Update()
-    {
-        if (hasAuthority)
-        {
-            var newY = startPosition.y + Mathf.Sin((Time.time + phaseOffset) * floatSpeed) * floatMagnitude;
-            transform.position = new Vector3(transform.position.x, newY, transform.position.z);
-        }
-    }
-
+    [Client]
     public void Select()
     {
-        if (!isOwned)
-            return;
+        if (!hasAuthority) { return; }
 
         onSelected?.Invoke();
     }
 
+    [Client]
     public void Deselect()
     {
-        if (!isOwned)
-            return;
+        if (!hasAuthority) { return; }
 
-        onDeselect?.Invoke();
+        onDeselected?.Invoke();
     }
 
     #endregion
